db1e320f8d9443578afaed32b4c13986d7eba403
hudson254 <hudsonwainaina@gmail.com>
2023-11-19 13:01:46 +0300

    final

---
+++ script.js
@@ -0,0 +1,14 @@
+const eyeIcon = document.querySelector(".eye-icon");
+
+const password = document.querySelector(".password");
+
+eyeIcon.addEventListener("click",()=>{
+
+    eyeIcon.classList.toggle("bx-show");
+
+    if(eyeIcon.classList.contains("bx-show")){
+        password.type = "text";
+    }else{
+        password.type = "password";
+    }
+})
